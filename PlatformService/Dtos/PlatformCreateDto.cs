68b3ffe422224a6b0dba9cabb5316ef4ada60da5
Bas Martens <bmartens1988@kpnmail.nl>
2022-01-06 13:39:13 +0100

    Initial commit of code

---
+++ PlatformService/Dtos/PlatformCreateDto.cs
@@ -0,0 +1,10 @@
+﻿namespace PlatformService.Dtos;
+
+public class PlatformCreateDto
+{
+    public string Name { get; set; } = default!;
+
+    public string Publisher { get; set; } = default!;
+
+    public string Cost { get; set; } = default!;
+}
---
+++ PlatformService/Models/Platform.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PlatformService.Models;
+
+public class Platform
+{
+    [Key] public int Id { get; set; }
+
+    public string Name { get; set; } = default!;
+
+    public string Publisher { get; set; } = default!;
+
+    public string Cost { get; set; } = default!;
+}
